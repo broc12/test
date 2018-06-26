d2bf744ebf7204057eacf6e0dfefb0ad903beb05
ggim00827 <ggim00827@gmail.com>
2018-06-26 16:27:50 +0900

    sd

--- Test/src/main/java/kkh/spring/test/HomeController.java
+++ Test/src/main/java/kkh/spring/test/HomeController.java
@@ -48,5 +48,19 @@ public class HomeController {
 		
 		return "home";
 	}
+	@RequestMapping(value = "/new", method = RequestMethod.GET)
+	public String indx2(Locale locale, Model model) {
+		logger.info("Welcome home! The client locale is {}.", locale);
+		
+		Date date = new Date();
+		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
+		
+		String formattedDate = dateFormat.format(date);
+		
+		model.addAttribute("serverTime", formattedDate );
+		
+		return "home";
+	}
+	
 	
 }
